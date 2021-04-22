cb034a5bb9c19dd2d94664e3dd0295ffc8861d5a
bonylumbanraja <bonylumbanraja@gmail.com>
2021-04-22 15:46:48 +0700

    Mengubah tampilan carousel ref #1

--- Nikah YUK!/Landing_Page.php
+++ Nikah YUK!/Landing_Page.php
@@ -471,11 +471,7 @@
                         </svg>
 
                         <div class="container">
-                            <div class="carousel-caption text-start">
-                                <h1>Example headline.</h1>
-                                <p>Some representative placeholder content for the first slide of the carousel.</p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-                            </div>
+                            
                         </div>
                     </div>
                     <div class="carousel-item">
